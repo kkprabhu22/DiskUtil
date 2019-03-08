b97e5edabacb5ed0c871202de504c0f36ca56453
kkprabhu22 <kirthiprabhu22@gmail.com>
2019-03-08 11:22:01 +0530

    Update Ex1.py

--- DiskUtil/Ex1.py
+++ DiskUtil/Ex1.py
@@ -3,16 +3,15 @@ import os
 def listfun(dir):
     filenames=os.listdir(dir)
     print("File name\t File path\t\t\t\t\tFile Size")
+
     for filename in filenames:
-
-        path=os.path.abspath(os.path.join(dir,filename))
-        size=os.stat(filename)
-
-        print(filename,"\t",path,"\t\t",size.st_size)
-
-
-
-
+        if(os.path.isdir(filename)):
+            print("directory")
+        else:
+                path=os.path.abspath(os.path.join(dir,filename))
+                size=os.stat(filename)
+                print(filename,"\t",path,"\t\t",size.st_size)
+                    
 def main():
     print("Select the options")
     print("1-Show current directory Details")
