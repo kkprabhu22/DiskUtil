c7e750acfd043bc5362b6dcf82dad42879754933
kkprabhu22 <kirthiprabhu22@gmail.com>
2019-03-08 17:59:30 +0530

    Update Ex1.py

--- DiskUtil/Ex1.py
+++ DiskUtil/Ex1.py
@@ -1,23 +1,33 @@
 import os
+import sys
+import time
 
 def listfun(dir):
     for root,dirs,files in os.walk(dir):
         for names in files:
             filename=names
             path=os.path.abspath(os.path.join(root,names))
-
-            print(filename,"\t\t\t\t\t\t",path)
-
+            size=os.stat(path).st_size
+            times=time.ctime(os.path.getmtime(path))
+            print("Filename -  ",filename)
+            print("File Absolute path -  ",path)
+            print("File Size in Bytes -  ",size)
+            print("File last Modification time -  ",times)
+            print("*******************************************************************************************")
 def main():
-    print("Select the options")
-    print("1-Show current Directory Details")
-    print("2-Show Parent Directory Details")
-    dir=input()
-    if(dir =="1"):
-        listfun(".")
-    elif(dir =="2"):
-        listfun("..")
-    else:
-        print("Invalid Option")
+    while True:
+         print("Select the options")
+         print("1-Show current Directory Details")
+         print("2-Show Parent Directory Details")
+         dir=input()
+         if(dir =="1"):
+             listfun(".")
+         elif(dir =="2"):
+             listfun("..")
+        #  elif(dir =="3"):
+        #      listfun(dir)
+        #      print("working oncode part")
+         else:
+             sys.exit(0)
 
 main()
